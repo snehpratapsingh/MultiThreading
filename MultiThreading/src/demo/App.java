86dffc51c8b9b52862524287cf221a971c77e31f
ssingh2 <ssingh2@DGA1APP60KNVQA.SWG.cdc.root>
2018-08-14 08:40:54 -0400

    Added runner class

--- MultiThreading/src/demo/App.java
+++ MultiThreading/src/demo/App.java
@@ -1,10 +1,22 @@
 package demo;
-
+class Runner extends Thread{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			try {
+				System.out.println("hello "+i);
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		Runner r1=new Runner();
+		r1.start();
 	}
 
 }
