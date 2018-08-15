bfc3f2990fa592568ee3cb14a09cbe6dab463ea4
ssingh2 <ssingh2@DGA1APP60KNVQA.SWG.cdc.root>
2018-08-15 03:59:44 -0400

    sync

--- MultiThreading/src/demo/SynchronizedThread.java
+++ MultiThreading/src/demo/SynchronizedThread.java
@@ -1,10 +1,43 @@
 package demo;
 
 public class SynchronizedThread {
-
+	private int count=0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SynchronizedThread st= new SynchronizedThread();
+		st.doWork();
+		
 	}
-
+	public void doWork() {
+		Thread t1=new Thread(new Runnable() {
+			public void run() {
+				for(int i=0; i<10000; i++) {
+					count++;
+				}
+			}
+		});
+		Thread t2=new Thread(new Runnable() {
+			public void run() {
+				for(int i=0; i<10000; i++) {
+					count++;
+				}
+			}
+		});
+		t1.start();
+		//this creates a new thread to run() returns immediately
+		//if we print value of count here, it would be 0
+		t2.start();
+		
+		try {
+			t1.join();
+			//join will wait for t1 thread to complete
+			//after it returns, next lines would execute
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("Count="+count);
+	}
 }
